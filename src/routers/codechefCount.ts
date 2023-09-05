61e9e741eb352a3db1ecbeda95994ffe0ad63ae8
siamsplash5 <siamsplash52@gmail.com>
2023-09-06 05:11:14 +0600

    fix: performance upgraded for spoj codeforces and codechef

--- src/routers/codechefCount.ts
+++ src/routers/codechefCount.ts
@@ -1,5 +1,6 @@
+import axios from "axios";
+import { load } from "cheerio";
 import express, { Request, Response } from "express";
-import puppeteer from "puppeteer";
 import responseHandler from "../handlers/response.handler";
 
 const codechefCount = express.Router();
@@ -9,32 +10,12 @@ codechefCount.get(
     async (req: Request, res: Response) => {
         try {
             const { username } = req.params;
-            const URL: string = `https://www.codechef.com/users/${username}`;
-            const xPath: string =
-                "/html/body/main/div/div/div/div/div/section[6]/div/h5[1]";
+            const profileURL: string = `https://www.codechef.com/users/${username}`;
 
-            // Launch Puppeteer browser and go to the URL
-            const browser = await puppeteer.launch();
-            const page = await browser.newPage();
-            await page.goto(URL);
+            const { data: html } = await axios.get(profileURL);
+            const $ = load(html);
+            const rawTxt = $('h5:contains("Fully Solved")').eq(0).text();
 
-            // Handle invalid username cases
-            if (page.url() !== URL) {
-                console.log("Invalid username");
-                throw new Error();
-            }
-
-            await page.waitForXPath(xPath);
-            const [el] = await page.$x(xPath);
-
-            // Get the text content of the element
-            const txtHandle = await el.getProperty("textContent");
-            const rawTxt: string = await txtHandle.jsonValue();
-
-            await browser.close();
-
-            // Convert the text like "Fully Solved (244)" to a number 244
-            // Use a regular expression to find the number within parentheses
             const match = rawTxt.match(/\((\d+)\)/);
             const totalSolved: number = parseInt(match[1], 10);
 
--- src/routers/codeforcesCount.ts
+++ src/routers/codeforcesCount.ts
@@ -1,5 +1,6 @@
+import axios from "axios";
+import { load } from "cheerio";
 import express, { Request, Response } from "express";
-import puppeteer from "puppeteer";
 import responseHandler from "../handlers/response.handler";
 
 const codeforcesCount = express.Router();
@@ -9,35 +10,18 @@ codeforcesCount.get(
     async (req: Request, res: Response) => {
         try {
             const { username } = req.params;
-            const URL: string = `https://codeforces.com/profile/${username}`;
-            const xPath: string =
-                '//*[@id="pageContent"]/div[4]/div/div[3]/div[1]/div[1]/div[1]';
-
-            // Launch Puppeteer browser and go to the URL
-            const browser = await puppeteer.launch();
-            const page = await browser.newPage();
-            await page.goto(URL);
-
-            // Handle invalid username cases
-            if (page.url() !== URL) {
-                console.log("Invalid username");
-                throw new Error();
-            }
-
-            await page.waitForXPath(xPath);
-            const [el] = await page.$x(xPath);
-
-            // Get the text content of the element
-            const txtHandle = await el.getProperty("textContent");
-            const rawTxt: string = await txtHandle.jsonValue();
-
-            // Convert the text like "1800 problems" or "1 problem" to number 1800 or 1
+            const profileURL: string = `https://codeforces.com/profile/${username}`;
+            const { data: html } = await axios.get(profileURL);
+            const $ = load(html);
+            const totalSolveTxt: string = $(
+                "div._UserActivityFrame_counterValue"
+            )
+                .eq(0)
+                .text();
             const totalSolved: number = parseInt(
-                rawTxt.replace(" problems" || " problem", ""),
+                totalSolveTxt.replace(" problems" || " problem", ""),
                 10
             );
-            await browser.close();
-
             responseHandler.success(res, "", { username, totalSolved });
         } catch (error) {
             console.error(error);
